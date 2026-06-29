7d220d922f256caef251e5a3a93f20eb14d8f0ec
ChibuikeNwobi <nwobichukwubuikem5@gmail.com>
2026-06-29 11:58:41 +0100

    First Push

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Switch, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'; // Import here instead
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/styles/colors";
+import Button from "./src/components/Button";
+import MyKeyboard from "./src/components/MyKeyboard";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "#000" }]
+        }
+      >
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === "light"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
